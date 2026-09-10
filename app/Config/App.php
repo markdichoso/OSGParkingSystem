9700d9e810de6cd45417f92c34e23ed25c11b42b
markdichoso <markdichoso@yahoo.com>
2026-09-10 16:30:39 +0800

    Updated $baseURL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = '';
+    public string $baseURL = 'https://osgcpms-eabqfpcjgxfwamcp.southeastasia-01.azurewebsites.net/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
