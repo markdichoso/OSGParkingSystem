bfa80ab3d455bd90df49de701485fd4eafe2eff9
markdichoso <markdichoso@yahoo.com>
2026-09-22 15:39:38 +0800

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -22,7 +22,7 @@ class Home extends BaseController
     {
         return view('main/register');
     }
-    public function authenticate()
+    public function authenticate(): string
     {
         $session = session();
 
